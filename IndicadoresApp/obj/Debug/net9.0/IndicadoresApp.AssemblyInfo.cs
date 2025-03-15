f888cfd4613f25a1377a082681d12560d6d14841
alex72hD <robinalexander544@gmail.com>
2025-03-15 15:21:25 -0500

    Se terminan los eventos onclick

--- IndicadoresApp/obj/Debug/net9.0/IndicadoresApp.AssemblyInfo.cs
+++ IndicadoresApp/obj/Debug/net9.0/IndicadoresApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IndicadoresApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0446daf5cb0687d7b0662277a36903129c9d52b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e836f88b8638750e98d4ccb7530d17e7b81aa843")]
 [assembly: System.Reflection.AssemblyProductAttribute("IndicadoresApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IndicadoresApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
