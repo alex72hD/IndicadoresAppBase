76961588e2cda5e90475c253d7330a9355c816c8
alex72hD <robinalexander544@gmail.com>
2025-03-29 13:41:42 -0500

    Se hace el get para indicador

--- IndicadoresApp/obj/Debug/net9.0/IndicadoresApp.AssemblyInfo.cs
+++ IndicadoresApp/obj/Debug/net9.0/IndicadoresApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IndicadoresApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d33edd6f495a9db6259e531dc9994d84c76ff66")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6565a42e00af98550c07325a0ca2e847f0a9ef1")]
 [assembly: System.Reflection.AssemblyProductAttribute("IndicadoresApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IndicadoresApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
