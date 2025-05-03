15b7d3ed234e3988015beea400bb8d5b207229bb
alex72hD <robinalexander544@gmail.com>
2025-05-03 10:02:32 -0500

    Se configuran las rutas y se hacen pruebas

--- IndicadoresApp/obj/Debug/net9.0/IndicadoresApp.AssemblyInfo.cs
+++ IndicadoresApp/obj/Debug/net9.0/IndicadoresApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IndicadoresApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e50a1854d14869845bafd5d8b8b205385f3abf1a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+394999c4a84efb53eda66b8a241e6ff154982d6c")]
 [assembly: System.Reflection.AssemblyProductAttribute("IndicadoresApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IndicadoresApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
