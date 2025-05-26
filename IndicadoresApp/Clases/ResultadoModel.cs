16f673dfdd77fe2d494946a88a70f06034ad9879
Alex72hD <robinalexander544@gmail.com>
2025-05-26 17:44:38 -0500

    Se agrega la navegacion para el usuario validador en las tablas con el crud sencillo

---
+++ IndicadoresApp/Clases/ResultadoModel.cs
@@ -0,0 +1,10 @@
+﻿namespace IndicadoresApp.Clases
+{
+    public class ResultadoModel
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Valor { get; set; } = string.Empty;
+        public DateTime FechaCalculo { get; set; } = DateTime.Now;
+        public string IndicadorId { get; set; } = string.Empty;
+    }
+}
